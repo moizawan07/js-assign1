82c76166e916233f1d272e7b80f45916ea662e32
moizawan07 <moizahmedawan321@gmail.com>
2024-09-04 00:53:32 -0700

    assignmnt1 js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var english = +prompt("Insert your english marks");
+var english = +prompt ("Insert your english marks");
 var urdu = +prompt ("Insert your urdu marks");
 var math = +prompt ("Insert your math marks");
 var chemistry = +prompt("Insert your chemistry marks");
@@ -6,23 +6,26 @@ var physics = +prompt("Insert your physics marks");
 
 var calculateMarks = english + urdu + math + chemistry + physics;
 
+var percentage = (calculateMarks*100)/500;
 
 
-if (calculateMarks >= 450 && calculateMarks < 500){
-    alert("Congarts! you getting A+ Grade")
+
+if (calculateMarks >=450 && calculateMarks <500){
+  alert("Marks" + calculateMarks + "\ngrade A+" + "\nPercentage" + percentage)
 }
-else if(calculateMarks >= 400 && calculateMarks < 450){
-    alert("Congrats! You getting A grade")
+else if(calculateMarks >=400 && calculateMarks < 450){
+    alert("Marks" + calculateMarks + "\ngrade A" + "\nPercentage" + percentage) 
 }
-else if(calculateMarks >= 350 && calculateMarks < 400){
-    alert("Congrtas you getting B grade")
+else if(calculateMarks >=350 && calculateMarks < 400){
+    alert("Marks" + calculateMarks + "\ngrade B" + "\nPercentage" + percentage) 
 }
-else if(calculateMarks >= 300 && calculateMarks < 350){
-    alert("Congarts you getting C grade")
+else if(calculateMarks >=300 && calculateMarks < 350){
+    alert("Marks" + calculateMarks + "\ngrade C" + "\nPercentage" + percentage) 
 }
-else if(calculateMarks >= 250 && calculateMarks < 300){
-    alert("Congrts you D grade")
+else if(calculateMarks >=250 && calculateMarks < 300){
+    alert("Marks" + calculateMarks + "\ngrade D" + "\nPercentage" + percentage) 
 }
 else{
-    alert("failed")
+    alert("FAILED")
 }
+
