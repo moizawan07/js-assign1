7342aa75ccb26841c47c1e4cedc969d5a84bc06e
moizawan07 <moizahmedawan321@gmail.com>
2024-09-04 22:11:54 -0700

    assignmnet-checker

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+var name=prompt("Enter your name")
+var rollNo= prompt("Enter your roll no")
 var english = +prompt ("Insert your english marks");
 var urdu = +prompt ("Insert your urdu marks");
 var math = +prompt ("Insert your math marks");
@@ -7,25 +10,30 @@ var physics = +prompt("Insert your physics marks");
 var calculateMarks = english + urdu + math + chemistry + physics;
 
 var percentage = (calculateMarks*100)/500;
-
+var grade;
 
 
 if (calculateMarks >=450 && calculateMarks <500){
-  alert("Marks" + calculateMarks + "\ngrade A+" + "\nPercentage" + percentage)
+    grade = "A+"
+}
+else if(calculateMarks >500){
+    alert("Please enter valid marks")
 }
 else if(calculateMarks >=400 && calculateMarks < 450){
-    alert("Marks" + calculateMarks + "\ngrade A" + "\nPercentage" + percentage) 
+    grade ="A"
 }
 else if(calculateMarks >=350 && calculateMarks < 400){
-    alert("Marks" + calculateMarks + "\ngrade B" + "\nPercentage" + percentage) 
+    grade = "B" 
 }
 else if(calculateMarks >=300 && calculateMarks < 350){
-    alert("Marks" + calculateMarks + "\ngrade C" + "\nPercentage" + percentage) 
+    grade = "C"
 }
 else if(calculateMarks >=250 && calculateMarks < 300){
-    alert("Marks" + calculateMarks + "\ngrade D" + "\nPercentage" + percentage) 
+    grade = "D"
 }
 else{
     alert("FAILED")
+    grade = "F"
 }
 
+alert("Name: "+ name + "\nRoll no: "+ rollNo + "\nMarks" + calculateMarks + "\ngrade "+ grade + "\nPercentage" + percentage);
